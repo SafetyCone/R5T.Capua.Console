d043c2b902c02f53aa6422bf0837dbf1c17478f7
MinexAutomation <minex.automation@gmail.com>
2020-05-02 16:45:04 -0700

    Initial commit of source-stub.

---
+++ source/Capua/Code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Capua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
